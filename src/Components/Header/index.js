1c3a483d915f9ac6e9714e1b5a22975ee4868a5e
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-02-04 18:06:03 +0100

    Make the header fixed positioned and add box-shadow to the header when scroll occurs

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,16 +3,43 @@ import "./index.css";
 
 import Filter from "./Filter";
 
-const Header = props => (
-  <div className="row">
-    <div className="d-flex main-header justify-content-between align-items-center">
-      <div className="d-flex">
-        <span className="main-header__title--name">Creatella</span>
-        <span className="main-header__title--slug">&nbsp;Faces</span>
-      </div>
-    </div>
-    <Filter />
-  </div>
-);
+export default class Header extends React.PureComponent {
+  state = {
+    boxShadow: false
+  };
 
-export default Header;
+  componentDidMount() {
+    document.body.onscroll = () => {
+      if (window.pageYOffset >= 70) {
+        this.setState({
+          boxShadow: true
+        });
+      } else {
+        this.setState({
+          boxShadow: false
+        });
+      }
+    };
+  }
+
+  render() {
+    let { boxShadow } = this.state;
+
+    return (
+      <div
+        className={`row main-header-container ${boxShadow &&
+          "main-header--shadow"}`}
+      >
+        <div
+          className={`d-flex main-header justify-content-between align-items-center`}
+        >
+          <div className="d-flex">
+            <span className="main-header__title--name">Creatella</span>
+            <span className="main-header__title--slug">&nbsp;Faces</span>
+          </div>
+        </div>
+        <Filter />
+      </div>
+    );
+  }
+}
