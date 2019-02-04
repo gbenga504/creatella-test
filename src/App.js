af2fedf129b1b911308530363cc9ae27966a0b00
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-02-04 18:15:59 +0100

    - Yarn add redux and react-redux - Rename Screens to Views in the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import "./App.css";
 
 import { Root } from "./Components/NotificationBanner";
+import HomeScreen from "./Views/HomeScreen";
+
+const store = createStore(() => ({}), applyMiddleware(thunk));
 
 export default class App extends React.PureComponent {
   render() {
-    return <Root />;
+    return (
+      <Provider store={store}>
+        <Root>
+          <HomeScreen />
+        </Root>
+      </Provider>
+    );
   }
 }
