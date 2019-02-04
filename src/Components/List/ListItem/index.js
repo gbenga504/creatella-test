ec055d7454c462f53c5fc0073d84f978c1ce0909
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-02-04 12:12:58 +0100

    Create a ListItem component

---
+++ src/Components/List/ListItem/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./index.css";
+
+const ListItem = props => (
+  <div className="d-flex flex-column list-item">
+    <div className="d-flex justify-content-between align-items-center">
+      <span className="list-item__title">#001</span>
+      <i className="mdi mdi-plus-circle" />
+    </div>
+    <span className="list-item__size">Available size (12)</span>
+    <span
+      className="align-self-center list-item__faces"
+      style={{ fontSize: 31 }}
+    >
+      ( .-. )
+    </span>
+    <span className="list-item__price">$3500</span>
+    <span className="list-item__date">3 days ago</span>
+  </div>
+);
+
+export default ListItem;
