a33c2fd3f0306d6d7743e3ee6ed293de03532061
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-02-04 18:09:20 +0100

    Create an HomeScreen view for the app and move content from App to HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import Header from "./Components/Header";
-import List from "./Components/List";
 import { Root } from "./Components/NotificationBanner";
 
 export default class App extends React.PureComponent {
   render() {
-    return (
-      <Root>
-        <div className="container-fluid">
-          <Header />
-          <div className="d-flex flex-column list-container">
-            <List />
-          </div>
-        </div>
-      </Root>
-    );
+    return <Root />;
   }
 }
---
+++ src/Screens/HomeScreen/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+import Header from "../../Components/Header";
+import List from "../../Components/List";
+
+export default class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header />
+        <div className="d-flex flex-column list-container">
+          <List />
+        </div>
+      </div>
+    );
+  }
+}
