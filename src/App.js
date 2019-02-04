766d95d3db202e8b03597d4c40f0a8efa5c16388
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-02-04 12:13:30 +0100

    Hook up the List component with the App component of the application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./App.css";
 
 import Header from "./Components/Header";
+import List from "./Components/List";
 
 export default class App extends React.PureComponent {
   render() {
     return (
       <div className="container-fluid">
         <Header />
+        <div className="d-flex flex-column list-container">
+          <span className="list-container__count-text">Viewing 5 faces</span>
+          <List />
+        </div>
       </div>
     );
   }
