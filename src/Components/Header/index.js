6fb67bb55d0f712cc7e7f22c000af7d66e49058d
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-02-05 16:23:37 +0100

    Add the cart number to the header of the application

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import "./index.css";
 
 import Filter from "./Filter";
 
-export default class Header extends React.PureComponent {
+class Header extends React.PureComponent {
   static propTypes = {
     onSelectFilter: PropTypes.func.isRequired
   };
@@ -19,9 +20,25 @@ export default class Header extends React.PureComponent {
             <span className="main-header__title--name">Creatella</span>
             <span className="main-header__title--slug">&nbsp;Faces</span>
           </div>
+          <div className="main-header__cart">
+            {this.props.cart.length > 0 && (
+              <div className="d-flex justify-content-center align-items-center main-header__cart-count">
+                {this.props.cart.length}
+              </div>
+            )}
+            <i className="mdi mdi-cart" />
+          </div>
         </div>
         <Filter onSetActiveFilter={_sort => this.props.onSelectFilter(_sort)} />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cart
+  };
+}
+
+export default connect(mapStateToProps)(Header);
